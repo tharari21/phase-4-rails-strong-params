d3915cdb0964d334f9784c3d2d90b944664c40a0
Tomer Harari <tharari21@gmail.com>
2022-08-30 19:56:23 -0400

    commit tree b2a95bca58133538306e19a4ecd489dc9563a54d

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,5 @@
 class BirdsController < ApplicationController
-
+  wrap_parameters format: []
   # GET /birds
   def index
     birds = Bird.all
@@ -8,7 +8,7 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
@@ -22,4 +22,9 @@ class BirdsController < ApplicationController
     end
   end
 
+  private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
 end
